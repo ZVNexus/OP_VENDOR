546a312dc3c011df294bb13d3e2baeb3795f91b1
Alexander Koskovich <zvnexus@outlook.com>
2020-03-19 06:16:37 -0400

    op3: Remove xtra_t_app & com.qualcomm.location.
     * CQL causes GNSS to constantly reboot, and removing
       xtra_t_app because it doesn't seem to affect anything.
    
    Change-Id: I08f9388a2a6bdd726b3b2178d7e77489173e1603

--- oneplus3/oneplus3.mk
+++ oneplus3/oneplus3.mk
@@ -21,10 +21,7 @@ PRODUCT_PACKAGES += \
     CneApp
 
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/proprietary/system/framework,$(TARGET_COPY_OUT_SYSTEM)/framework) \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/proprietary/system/lib,$(TARGET_COPY_OUT_SYSTEM)/lib) \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64) \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/proprietary/system,$(TARGET_COPY_OUT_SYSTEM)) \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/proprietary/vendor/bin,$(TARGET_COPY_OUT_VENDOR)/bin) \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/proprietary/vendor/etc,$(TARGET_COPY_OUT_VENDOR)/etc) \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/proprietary/vendor/firmware,$(TARGET_COPY_OUT_VENDOR)/firmware) \
